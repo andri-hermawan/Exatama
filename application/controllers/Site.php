341ae7cc89bed2a36a22ec8db5f4abd0faafc680
andri-hermawan <andri.hermawan.skom@gmail.com>
2016-01-05 16:45:43 +0700

    Edit Front End Produk (CSS)

---
+++ application/controllers/Site.php
@@ -0,0 +1,117 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Site extends CI_Controller {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('currency_format_helper');
+	}
+
+	
+	public function index($offset=0)
+	{
+		$this->countervisitor();
+		/* Pagination */
+		$config['uri_segment'] = 3;
+		$config['base_url'] = base_url().'site/index';
+		$config['total_rows'] = $this->model->count_all();
+		$config['per_page'] = 6;
+
+		/*============================== ambil query database ==================*/
+		$data['data_produk']=$this->model->GetProduk("where tb_produk.status = 'publish' group by tb_produk.id_produk order by tb_produk.id_produk desc limit ".$config['per_page']." offset ".$offset)->result_array();
+		$data['rekomen'] = $this->model->GetProduk("where status = 'publish' order by rand() limit 3")->result_array();
+		/*======================================================================*/
+
+                // CSS Bootstrap               
+		$config['full_tag_open'] = '<ul class="pagination">';
+		$config['full_tag_close'] = '</ul>';            
+		$config['prev_link'] = '«';
+		$config['prev_tag_open'] = '<li>';
+		$config['prev_tag_close'] = '</li>';
+		$config['next_link'] = '»';
+		$config['next_tag_open'] = '<li>';
+		$config['next_tag_close'] = '</li>';
+		$config['cur_tag_open'] = '<li class="active"><a href="">';
+		$config['cur_tag_close'] = '</a></li>';
+		$config['num_tag_open'] = '<li>';
+		$config['num_tag_close'] = '</li>';
+                // Akhir CSS
+
+		$config["num_links"] = round( $config["total_rows"] / $config["per_page"] );           
+		$this->pagination->initialize($config);
+		$data['pages'] = $this->pagination->create_links();
+
+		
+
+		$this->load->view('layout/header',$data);
+		$this->load->view('site/index', $data);
+		$this->load->view('layout/slidebar', $data);
+		$this->load->view('layout/footer', $data);
+	}
+
+	private function countervisitor(){
+
+		if($this->agent->is_browser()){
+			$agent = $this->agent->browser().' '.$this->agent->version();
+		}elseif ($this->agent->is_robot()){
+			$agent = $this->agent->robot();
+		}elseif ($this->agent->is_mobile()){
+			$agent = $this->agent->mobile();
+		}else{
+			$agent = 'Unidentified User Agent';
+		}
+		
+		$data_visitor = $this->model->GetVisitor("where ip = '".$_SERVER['REMOTE_ADDR']."'")->result_array();
+		if($data_visitor == NULL){
+			$data = array(
+				'ip' => $_SERVER['REMOTE_ADDR'],
+				'os' => $this->agent->platform(),
+				'browser' => $agent,
+				'tanggal' => date("Y-m-d H:i:s")
+			);
+			$this->model->Simpan('tb_visitor',$data);
+			$this->session->set_userdata('pasartungging',"Ridho");
+			setcookie("pasartungging",'Ridho', time()+3600*24);
+		}else{
+			if(!isset($_COOKIE['pasartungging'])){
+				$data_visitor = $this->model->GetVisitor("where ip = '".$_SERVER['REMOTE_ADDR']."' and tanggal = '".date("Y-m-d H:i:s")."'");
+				if($data_visitor != NULL){
+					if(!$this->session->userdata('pasartungging.com')){
+						$data = array(
+							'ip' => $_SERVER['REMOTE_ADDR'],
+							'os' => $this->agent->platform(),
+							'browser' => $agent,
+							'tanggal' => date("Y-m-d H:i:s")
+						);
+						$this->model->Simpan('tb_visitor', $data);
+						$this->session->set_userdata('pasartungging',"Ridho");
+						setcookie("pasartungging",'Ridho', time()+3600*24);
+					}else{
+						setcookie("pasartungging",'Ridho', time()+3600*24);
+					}
+				}else{
+					$data = array(
+							'ip' => $_SERVER['REMOTE_ADDR'],
+							'os' => $this->agent->platform(),
+							'browser' => $agent,
+							'tanggal' => date("Y-m-d H:i:s")
+					);
+					$this->model->Simpan('tb_visitor', $data);
+					$this->session->set_userdata('pasartungging',"Ridho");
+					setcookie("pasartungging",'Ridho', time()+3600*24);
+				}
+			}
+		}
+	}
+
+	function sidebar_kat(){
+		$data = array(
+			"total_kat" => $this->model->TotalKat('')->result_array(),
+			"total_merk" => $this->model->GetProduk('group by id_merk')->num_rows(),
+			"kategoriq" => $this->model->GetKat()->result_array(),
+			"merk" => $this->model->GetMerk()->result_array(),
+			);
+		return $this->load->view("layout/slidebar", $data, TRUE);
+	}
+}
--- application/views/site/index.php
+++ application/views/site/index.php
@@ -37,17 +37,20 @@
 	   		     	  <a class="view-all" href="product.html">VIEW ALL<span> </span></a> 		     
 	   		     <div class="clearfix"> </div>
 				 </div>
+
 	   		     <div class="product-left">
+	   		     	 <?php foreach($data_produk as $row) { ?>
+	   		     	
 	   		     	<div class="col-md-4 chain-grid">
-	   		     		<a href="single.html"><img class="img-responsive chain" src="assets/site/images/ch.jpg" alt=" " /></a>
+	   		     		<a href="single.html"><img class="img-responsive chain" src="<?php echo base_url(); ?>assets/admin/upload/<?php echo $row['foto']; ?>" alt=" " /></a>
 	   		     		<span class="star"> </span>
 	   		     		<div class="grid-chain-bottom">
-	   		     			<h6><a href="single.html">Lorem ipsum dolor</a></h6>
+	   		     			<h6><a href="single.html"><?php echo $row['judul']; ?></a></h6>
 	   		     			<div class="star-price">
 	   		     				<div class="dolor-grid"> 
 		   		     				<span class="actual">300$</span>
-		   		     				<span class="reducedfrom">400$</span>
-		   		     				  <span class="rating">
+		   		     				<span class="reducedfrom"><?php echo currency_format($row['harga']); ?></span>
+		   		     				 <!--  <span class="rating">
 									        <input type="radio" class="rating-input" id="rating-input-1-5" name="rating-input-1">
 									        <label for="rating-input-1-5" class="rating-star1"> </label>
 									        <input type="radio" class="rating-input" id="rating-input-1-4" name="rating-input-1">
@@ -58,73 +61,28 @@
 									        <label for="rating-input-1-2" class="rating-star"> </label>
 									        <input type="radio" class="rating-input" id="rating-input-1-1" name="rating-input-1">
 									        <label for="rating-input-1-1" class="rating-star"> </label>
-							    	   </span>
+							    	   </span> -->
 	   		     				</div>
-	   		     				<a class="now-get get-cart" href="#">ADD TO CART</a> 
+	   		     				<a class="now-get get-cart" href="<?php echo base_url(); ?>detail-<?php echo strtolower(str_replace(' ','-',preg_replace("/[^a-zA-Z0-9\s]/", "", $row['judul']))).'-'.$row['id_produk']; ?>">Lihat Detail</a> 
 	   		     				<div class="clearfix"> </div>
 							</div>
 	   		     		</div>
 	   		     	</div>
-	   		     	<div class="col-md-4 chain-grid">
-	   		     		<a href="single.html"><img class="img-responsive chain" src="assets/site/images/ba.jpg" alt=" " /></a>
-	   		     		<span class="star"> </span>
-	   		     		<div class="grid-chain-bottom">
-	   		     			<h6><a href="single.html">Lorem ipsum dolor</a></h6>
-	   		     			<div class="star-price">
-	   		     				<div class="dolor-grid"> 
-		   		     				<span class="actual">300$</span>
-		   		     				<span class="reducedfrom">400$</span>
-		   		     				  <span class="rating">
-									        <input type="radio" class="rating-input" id="rating-input-1-5" name="rating-input-1">
-									        <label for="rating-input-1-5" class="rating-star1"> </label>
-									        <input type="radio" class="rating-input" id="rating-input-1-4" name="rating-input-1">
-									        <label for="rating-input-1-4" class="rating-star1"> </label>
-									        <input type="radio" class="rating-input" id="rating-input-1-3" name="rating-input-1">
-									        <label for="rating-input-1-3" class="rating-star"> </label>
-									        <input type="radio" class="rating-input" id="rating-input-1-2" name="rating-input-1">
-									        <label for="rating-input-1-2" class="rating-star"> </label>
-									        <input type="radio" class="rating-input" id="rating-input-1-1" name="rating-input-1">
-									        <label for="rating-input-1-1" class="rating-star"> </label>
-							    	   </span>
-	   		     				</div>
-	   		     				<a class="now-get get-cart" href="#">ADD TO CART</a> 
-	   		     				<div class="clearfix"> </div>
-							</div>
-	   		     		</div>
-	   		     	</div>
-	   		     	<div class="col-md-4 chain-grid grid-top-chain">
-	   		     		<a href="single.html"><img class="img-responsive chain" src="assets/site/images/bo.jpg" alt=" " /></a>
-	   		     		<span class="star"> </span>
-	   		     		<div class="grid-chain-bottom">
-	   		     			<h6><a href="single.html">Lorem ipsum dolor</a></h6>
-	   		     			<div class="star-price">
-	   		     				<div class="dolor-grid"> 
-		   		     				<span class="actual">300$</span>
-		   		     				<span class="reducedfrom">400$</span>
-		   		     				  <span class="rating">
-									        <input type="radio" class="rating-input" id="rating-input-1-5" name="rating-input-1">
-									        <label for="rating-input-1-5" class="rating-star1"> </label>
-									        <input type="radio" class="rating-input" id="rating-input-1-4" name="rating-input-1">
-									        <label for="rating-input-1-4" class="rating-star1"> </label>
-									        <input type="radio" class="rating-input" id="rating-input-1-3" name="rating-input-1">
-									        <label for="rating-input-1-3" class="rating-star"> </label>
-									        <input type="radio" class="rating-input" id="rating-input-1-2" name="rating-input-1">
-									        <label for="rating-input-1-2" class="rating-star"> </label>
-									        <input type="radio" class="rating-input" id="rating-input-1-1" name="rating-input-1">
-									        <label for="rating-input-1-1" class="rating-star"> </label>
-							    	   </span>
-	   		     				</div>
-	   		     				<a class="now-get get-cart" href="#">ADD TO CART</a> 
-	   		     				<div class="clearfix"> </div>
-							</div>
-	   		     		</div>
-	   		     	</div>
+	   		     	 <?php } ?>
+	   		     	 <div class="pagination-area">
+							<ul class="pagination">
+								<?php echo $pages; ?>
+							</ul>
+					</div>
 	   		     	 <div class="clearfix"> </div>
 	   		     </div>
 	   		     <div class="clearfix"> </div>
-				 
+				
 	   		   </div>   
+
+	   		   
+
+
 			  
 	
 	<!---->
-	
